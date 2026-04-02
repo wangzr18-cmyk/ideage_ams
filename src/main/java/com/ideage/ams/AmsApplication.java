b787870aac0daded1f55c8b49379f94aa1a8ce12
wangzhengrong <wangzr18@gmail.com>
2026-04-02 13:29:57 +0900

    test Jenkins1

--- src/main/java/com/ideage/ams/AmsApplication.java
+++ src/main/java/com/ideage/ams/AmsApplication.java
@@ -10,7 +10,7 @@ public class AmsApplication {
 
     public static void main(String[] args) {
         System.out.println("ams starting...");
-        System.out.println("Jenkins Test...");
+        System.out.println("Jenkins starting...");
         SpringApplication.run(AmsApplication.class, args);
     }
 }
